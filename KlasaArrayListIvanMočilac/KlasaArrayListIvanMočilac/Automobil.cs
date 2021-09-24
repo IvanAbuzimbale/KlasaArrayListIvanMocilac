d69e89541c97044790aef77c59745fadd15753ba
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-09-24 22:09:07 +0200

    Add files via upload

---
+++ KlasaArrayListIvanMočilac/KlasaArrayListIvanMočilac/Automobil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaArrayListIvanMočilac
+{
+    class Automobil
+    {
+        private string marka;
+
+        public Automobil(string m){
+            marka = m;
+        }
+
+        public string DajMarku(){
+            return marka;
+        }
+    }
+}
---
+++ KlasaArrayListIvanMočilac/KlasaArrayListIvanMočilac/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace KlasaArrayListIvanMočilac
+{
+    public partial class Form1 : Form
+    {
+        ArrayList listaAutomobila = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNovi_Click(object sender, EventArgs e)
+        {
+            textBoxNovi.Enabled = true;
+            buttonUnesi.Enabled = true;
+        }
+
+        private void buttonUnesi_Click(object sender, EventArgs e)
+        {
+            Automobil a = new Automobil(textBoxNovi.Text);
+            listaAutomobila.Add(a);
+            richTextBox1.AppendText(listaAutomobila.IndexOf(a) + 1 + ". " + a.DajMarku() + "\n");
+        }
+
+        private void buttonObrisi_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
